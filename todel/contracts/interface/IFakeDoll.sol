6308145b7e5a1a26a704d28da8b231bfd4dfa614
LouGel <l.gelinet@prontonmail.ch>
2022-07-19 14:15:05 +0200

    Mistake env

--- todel/contracts/interface/IFakeDoll.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface IFakeDoll is IERC20 {
-    //0xa0712d68
-    function mint(uint256 nb) external;
-}
